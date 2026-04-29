c5866bfdefebd99f63cca3726f1371313a88954d
PH Hardware <sales@phhardware.com>
2026-04-29 18:33:15 +0800

    Fix: ensure automatic redirect to /en

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,12 @@
-import createMiddleware from "next-intl/middleware"; export default createMiddleware({ locales: ["en", "es", "fr", "ru"], defaultLocale: "en" }); export const config = { matcher: ["/((?!api|_next|.*\\..*).*)"] };
+import createMiddleware from 'next-intl/middleware';
+ 
+export default createMiddleware({
+  locales: ['en', 'es', 'fr', 'ru'],
+  defaultLocale: 'en',
+  localePrefix: 'always'
+});
+ 
+export const config = {
+  // Skip all paths that should not be internationalized
+  matcher: ['/((?!api|_next|.*\\..*).*)']
+};
