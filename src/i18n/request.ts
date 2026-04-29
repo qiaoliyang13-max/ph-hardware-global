11a3cd13a0a6cf08d051d13526894aa9a1fd9027
PH Hardware <sales@phhardware.com>
2026-04-29 18:58:27 +0800

    Fix: Ultimate path stabilization and i18n relocation

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,13 +1,1 @@
-import {getRequestConfig} from 'next-intl/server';
- 
-export default getRequestConfig(async ({locale}) => {
-  let messages;
-  try {
-    messages = (await import(`../../messages/${locale}.json`)).default;
-  } catch (error) {
-    messages = (await import(`../../messages/en.json`)).default;
-  }
-  return {
-    messages
-  };
-});
+import {getRequestConfig} from "next-intl/server"; export default getRequestConfig(async ({locale}) => ({ messages: (await import(`./messages/${locale}.json`)).default }));
